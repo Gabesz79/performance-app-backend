5f07b71319abec5dc6aa2693e97aa586e90d914f
Gabesz79 <58592305+Gabesz79@users.noreply.github.com>
2025-09-19 12:15:54 +0200

    Feat/boot skeleton (#2)
    * build: add Spring Boot (Gradle Groovy) skeleton
    
    * ci: add GitHub Actions build (JDK 21, Gradle)
    
    * ci: make gradlew executable in CI

---
+++ src/test/java/com/gabesz79/performance_app_backend/PerformanceAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabesz79.performance_app_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerformanceAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
