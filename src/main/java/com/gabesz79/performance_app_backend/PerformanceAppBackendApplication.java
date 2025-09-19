535373e8c0ebb6f8e4e895f4cf75220364bc270a
Gabesz79 <58592305+Gabesz79@users.noreply.github.com>
2025-09-19 11:19:31 +0200

    build: add Spring Boot (Gradle Groovy) skeleton

---
+++ src/main/java/com/gabesz79/performance_app_backend/PerformanceAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.gabesz79.performance_app_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerformanceAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PerformanceAppBackendApplication.class, args);
+	}
+
+}
